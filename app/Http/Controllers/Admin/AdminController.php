f35735d9aff67a08c329573eb3741c0e9f33059b
daniyalbutt <34391174+daniyalbutt@users.noreply.github.com>
2025-04-09 22:09:45 +0500

    Adding Guide Count

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -30,7 +30,8 @@ class AdminController extends Controller
         $tag_count = DB::table('tags')->where('status', 0)->count();
         $user_count = DB::table('users')->where('is_admin', 1)->where('is_company', 0)->count();
         $document_count = DB::table('documents')->where('status', 0)->count();
-        return view('admin.home', compact('company_count', 'tag_count', 'user_count', 'document_count'));
+        $guide_count = DB::table('guides')->where('status', 0)->count();
+        return view('admin.home', compact('company_count', 'tag_count', 'user_count', 'document_count', 'guide_count'));
     }
 
     public function markAsRead(){
