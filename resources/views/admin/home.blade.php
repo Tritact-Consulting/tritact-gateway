8cb3d14e601062b9404226467a3dd505ec375424
daniyalbutt <daniyalbutt785@gmail.com>
2025-06-13 03:32:15 +0500

    Update home.blade.php

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -170,7 +170,7 @@
         <div class="col-12">
 			<div class="box">
                 <div class="box-header with-border">
-                    <h4 class="box-title">Assign Audit</h4>
+                    <h4 class="box-title">Live Audit</h4>
                 </div>
 			    <div class="box-body">
 				    <div class="table-responsive rounded card-table">
